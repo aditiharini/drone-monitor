a86573d178a620e770bf62993e6e15b965224048
Aditi Srinivasan <aditiharini@gmail.com>
2020-08-28 21:09:23 +0000

    test all endpoints

--- api/drone_status.go
+++ api/drone_status.go
@@ -1,6 +1,9 @@
 package api
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 type State struct {
 	Drone struct {
@@ -14,9 +17,12 @@ type State struct {
 type Point [2]float64
 
 type DjiState struct {
+	ID             int        `json:"id"`
+	Name           string     `json:"name"`
 	Location       Point      `json:"location"`
 	Battery        float32    `json:"battery"` // fraction of available battery, range [0,1]
 	GPS            [3]float64 `json:"gps"`
+	XYZ            [3]float64 `json:"xyz"`
 	Yaw            float32    `json:"yaw"`
 	BatteryVoltage float32    `json:"batteryVoltage"`
 	LastSeenTime   int64      `json:"last_seen_time"`
@@ -35,13 +41,24 @@ type SaturatrState struct {
 }
 
 func (s *State) HandleDji(res http.ResponseWriter, req *http.Request) {
-
+	fmt.Println("Post request from dji")
 }
 
 func (s *State) HandleDroneSaturatr(res http.ResponseWriter, req *http.Request) {
-
+	fmt.Println("Post request from drone saturatr")
 }
 
 func (s *State) HandleServerSaturatr(res http.ResponseWriter, req *http.Request) {
+	req.FormValue("acker_packets_sent")
+	fmt.Printf(
+		"Post request from drone saturatr (acker_packets_sent: %s, acker_packets_received: %s, saturatr_packets_sent: %s, saturatr_packets_received: %s\n",
+		req.FormValue("acker_packets_sent"),
+		req.FormValue("acker_packets_received"),
+		req.FormValue("saturatr_packets_sent"),
+		req.FormValue("saturatr_packets_received"),
+	)
+}
 
+func (s *State) HandleGetState(res http.ResponseWriter, req *http.Request) {
+	fmt.Println("Get request from client")
 }
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 
 	"github.com/aditiharini/drone-monitor/api"
@@ -9,7 +11,12 @@ import (
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	state := api.State{}
-	router.HandleFunc("/drone/flight", state.HandleDji)
-	router.HandleFunc("/drone/saturatr", state.HandleDroneSaturatr)
-	router.HandleFunc("/server/saturatr", state.HandleServerSaturatr)
+	router.HandleFunc("/drone/flight", state.HandleDji).Methods("POST")
+	router.HandleFunc("/drone/saturatr", state.HandleDroneSaturatr).Methods("POST")
+	router.HandleFunc("/server/saturatr", state.HandleServerSaturatr).Methods("POST")
+	router.HandleFunc("/state", state.HandleGetState).Methods("GET")
+	err := http.ListenAndServe(":10000", router)
+	if err != nil {
+		panic(err)
+	}
 }
