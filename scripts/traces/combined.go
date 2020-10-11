ac545814100ec1d456eddae5cc855acbbad63d5a
Aditi Srinivasan <aditiharini@gmail.com>
2020-10-11 22:26:52 +0000

    add handling for -1 data

--- scripts/traces/combined.go
+++ scripts/traces/combined.go
@@ -26,12 +26,34 @@ type LogLine struct {
 func (ll *LogLine) ToCsvRow() []string {
 	timeStr := strconv.FormatInt(ll.Time.Unix(), 10)
 	rsrp := ll.State.Drone.Signal.Rsrp
+	if rsrp == "-1" {
+		rsrp = "NA"
+	}
 	rsrq := ll.State.Drone.Signal.Rsrq
+	if rsrq == "-1" {
+		rsrq = "NA"
+	}
 	rssi := ll.State.Drone.Signal.Rssi
+	if rssi == "-1" {
+		rssi = "NA"
+	}
 	sinr := ll.State.Drone.Signal.Sinr
+	if sinr == "-1" {
+		sinr = "NA"
+	}
 	uplinkBandwidth := fmt.Sprintf("%f", ll.State.Server.Iperf.Bandwidth)
+	if ll.State.Server.Iperf.Bandwidth == -1 {
+		uplinkBandwidth = "NA"
+	}
+
 	downlinkBandwidth := fmt.Sprintf("%f", ll.State.Drone.Iperf.Bandwidth)
+	if ll.State.Drone.Iperf.Bandwidth == -1 {
+		downlinkBandwidth = "NA"
+	}
 	latency := fmt.Sprintf("%f", ll.State.Drone.Ping.Latency)
+	if ll.State.Drone.Ping.Latency == -1 {
+		latency = "NA"
+	}
 	// TODO(aditi): change bw and latency to be parsed as strings
 	return []string{timeStr, rsrp, rsrq, rssi, sinr, uplinkBandwidth, downlinkBandwidth, latency}
 }
